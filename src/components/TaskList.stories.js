3a1b05791c5df660237461f4aad08886fee723dc
Atin Tanwar <atin1993dectanwar@gmail.com>
2022-02-12 11:55:09 +0530

    changes

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -16,12 +16,12 @@ Default.args = {
   // Shaping the stories through args composition.
   // The data was inherited from the Default story in Task.stories.js.
   tasks: [
-    { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
-    { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
-    { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
-    { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
-    { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
-    { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+    { ...TaskStories.Default.args.task, id: '1', title: 'Task new  1' },
+    { ...TaskStories.Default.args.task, id: '2', title: 'Task new  2' },
+    { ...TaskStories.Default.args.task, id: '3', title: 'Task new 3' },
+    { ...TaskStories.Default.args.task, id: '4', title: 'Task new 4' },
+    { ...TaskStories.Default.args.task, id: '5', title: 'Task new 5' },
+    { ...TaskStories.Default.args.task, id: '6', title: 'Task new 6' },
   ],
 };
 
